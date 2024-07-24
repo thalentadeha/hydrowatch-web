511895c4321137ab3a72fea7e77a446950fa1dcf
Muhammad Thalenta Dirgantara Deha <92012703+thalentadeha@users.noreply.github.com>
2024-07-24 20:43:12 +0700

    admin, user, login middleware implement

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+            'user' => \App\Http\Middleware\UserMiddleware::class,
+            'login' => \App\Http\Middleware\LoginMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
