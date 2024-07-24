28e306ef4c64d0426eed3de1074107a990bb7ee2
Muhammad Thalenta Dirgantara Deha <92012703+thalentadeha@users.noreply.github.com>
2024-07-24 20:42:36 +0700

    admin, user, login middleware

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Kreait\Firebase\Contract\Auth;
+use Kreait\Firebase\Contract\Database;
+use Kreait\Firebase\Contract\Firestore;
+use Kreait\Firebase\Factory;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+
+     protected $auth;
+    protected $db;
+    protected $database;
+
+     public function __construct(Firestore $firestore, Auth $auth, Database $database)
+    {
+        $this->auth = $auth;
+        // $this->db = $firestore->database();
+        $this->database = $database;
+
+        $firestore = app('firebase.firestore');
+        $this->db = $firestore->database();
+    }
+
+    public function handle(Request $request, Closure $next): Response
+    {
+        try {
+            $idToken = session('idToken');
+            $verifiedIdToken = $this->auth->verifyIdToken($idToken);
+            $uid = $verifiedIdToken->claims()->get('sub');
+            // $userDoc = $this->firestore->collection('users')->document($uid)->snapshot();
+            $snapshot = $this->database->getReference('users')->getChild($uid)->getSnapshot();
+            $userDoc = $snapshot->getValue();
+
+            if (!$userDoc['isAdmin']) {
+                return redirect()->route('user-dashboard')->withErrors(['error' => 'Unauthorized']);
+            }
+
+            return $next($request);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return redirect()->route('login')->withErrors(['error' => 'Unauthorized']);
+        }
+    }
+}
---
+++ app/Http/Middleware/LoginMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Kreait\Firebase\Contract\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+
+     protected $auth;
+
+     public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+    public function handle(Request $request, Closure $next): Response
+    {
+        $idToken = session('idToken');
+
+        if (!$idToken) {
+            return redirect()->route('login_GET')->withErrors(['error' => 'You need to log in.']);
+        }
+
+        return $next($request);
+    }
+}
