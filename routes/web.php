96b1d05e4c3ef05de94e9df016ef124a1eb7f830
Muhammad Thalenta Dirgantara Deha <92012703+thalentadeha@users.noreply.github.com>
2024-07-24 20:47:18 +0700

    route for user authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use DebugBar\DebugBar;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AuthController::class, 'showLogin'])->name('login_GET');
+Route::post('/login', [AuthController::class, 'login'])->name('login_POST');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout_POST');
+
+Route::middleware(['login', 'admin'])->group(function () {
+    Route::get('admin/dashboard', function(){
+        return view('admin.dashboard');
+    })->name('admin-dashboard');
+
+    //user management
+    Route::get('admin/register-user', [AuthController::class, 'showRegister'])->name('register_GET');
+    Route::post('admin/register-user', [AuthController::class, 'register'])->name('register_POST');
 });
